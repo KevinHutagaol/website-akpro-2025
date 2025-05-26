4dc696bd3a67355496d547747c0b02d5a7858623
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-05-26 19:44:45 +0700

    Gimmick easter egg lmao

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [react()],
   adapter: netlify(),
-  // redirects: {
-  //   '/diktat-asistensi/jadwal-asistensi' : 'https://docs.google.com/spreadsheets/d/163hVaMOp6ueTPgI_qtoAmQ2mu7z_FvGspG9_36Cffxs/edit?gid=0#gid=0'
-  // },
+  redirects: {
+    '/diktat-asistensi/jadwal-asistensi' : '/TOP-SECRET-PAGE-DO-NOT-OPEN/',
+    '/diktat-asistensi' : '/TOP-SECRET-PAGE-DO-NOT-OPEN/',
+    '/diktat-asistensi/kumpulan-diktat' : '/TOP-SECRET-PAGE-DO-NOT-OPEN/',
+  },
   env: {
     schema: {
       YOUTUBE_URL: envField.string({context: "client", access: "public"})
