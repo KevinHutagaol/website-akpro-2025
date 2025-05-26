3cfd4dfec59f81e6c0b52198ae1c0705fc7eca14
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-05-26 09:29:08 +0700

    Finished CalendarAsistensi (some todos left)

--- src/components/jadwal-asistensi/CalendarAsistensi.tsx
+++ src/components/jadwal-asistensi/CalendarAsistensi.tsx
@@ -1,4 +1,10 @@
-import { useMemo } from "react"
+import {
+    type CSSProperties,
+    useEffect,
+    useMemo,
+    useRef,
+    useState
+} from "react"
 import "/src/styles/global.css"
 import styles from "/src/styles/CalendarAsistensi.module.css"
 
@@ -29,6 +35,9 @@ type AsisData = {
     recordingsLink?: string;
 }
 
+type DisplayData = { date: Date, contents: Array<{ data: AsisData, isHighlighted: boolean }> };
+
+
 const getDaysArray = function (start: Date, end: Date) {
     const arr: Array<Date> = [];
     let currentDate = new Date(start);
@@ -49,8 +58,193 @@ const getDaysArray = function (start: Date, end: Date) {
     return arr;
 };
 
+const convertToTerm = (year: number, ganjil_genap: ("ganjil" | "genap")): number => ganjil_genap === 'genap' ? 2 * year : 2 * year - 1;
+
+interface InternalMemberItemProps {
+    entry: DisplayData | null;
+    ganjil_genap: "genap" | "ganjil";
+}
+
+function InternalMemberItem(props: InternalMemberItemProps) {
+    type PopupDirection = "up" | "down";
+
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [popupDirection, setPopupDirection] = useState<PopupDirection>("down");
+    const [activeItemData, setActiveItemData] = useState<AsisData | null>(null);
+    const [activeElementIndex, setActiveElementIndex] = useState<Number | null>(null);
+
+    const popupRef = useRef<HTMLDivElement | null>(null);
+    const elementRef = useRef<HTMLDivElement | null>(null);
+
+    const findPopupDirection = () => {
+        if (!elementRef.current || !popupRef.current) return "bottom" as PopupDirection;
+
+        const elementRect = elementRef.current.getBoundingClientRect();
+
+        const upperAvailableSpace = elementRect.top;
+        const lowerAvailableSpace = window.innerHeight - elementRect.bottom;
+
+        if (upperAvailableSpace > lowerAvailableSpace) {
+            return "up";
+        } else {
+            return "down";
+        }
+    }
+
+    const handleMouseEnter = (itemData: AsisData, index: number) => {
+        setActiveItemData(itemData);
+        setActiveElementIndex(index);
+
+        if (!isPopupVisible) {
+            setIsPopupVisible(true);
+        } else {
+            const newDirection = findPopupDirection();
+            if (newDirection !== popupDirection) {
+                setPopupDirection(newDirection);
+            }
+        }
+    }
+
+    const handleMouseLeave = () => {
+        setIsPopupVisible(false);
+        setActiveItemData(null);
+        setActiveElementIndex(null);
+    }
+
+    useEffect(() => {
+        if (isPopupVisible && activeItemData) {
+            const reevaluateDirection = () => {
+                const newDirection = findPopupDirection();
+                if (newDirection !== popupDirection) {
+                    setPopupDirection(newDirection);
+                }
+            };
+            reevaluateDirection();
+
+            window.addEventListener("resize", reevaluateDirection);
+            window.addEventListener("scroll", reevaluateDirection, true);
+
+            return () => {
+                window.removeEventListener("resize", reevaluateDirection);
+                window.removeEventListener("scroll", reevaluateDirection, true);
+            }
+        }
+    }, [isPopupVisible, activeItemData, popupDirection]);
+
+    const popupDirectionClass = popupDirection === 'up' ? styles.item__popup_up_cell : styles.item__popup_down_cell;
+
+    return (
+        <div className={styles.calendar__contents}
+             ref={elementRef}
+             onMouseLeave={handleMouseLeave}
+        >
+            {props.entry === null ? null : (
+                props.entry!.contents.map((item, i) => {
+
+                    let triggerElementClass = '';
+                    if (isPopupVisible && activeElementIndex === i) {
+                        triggerElementClass = popupDirection === "up" ? styles.item__internal_member__active__up : styles.item__internal_member__active__down;
+                    }
+
+                    return (
+                        // TODO: Border for multi-item currently not fully working (good enough)
+                        <div
+                            className={`${styles.item__internal_member} ${(isPopupVisible && activeElementIndex === i) ? triggerElementClass : ''}`}
+                            style={{
+                                "--_side-strip-color": item.isHighlighted ? "orangered" : "",
+                                opacity: item.isHighlighted ? 1 : 0.5
+                            } as CSSProperties}
+                            onMouseEnter={() => handleMouseEnter(item.data, i)}
+                            onFocus={() => handleMouseEnter(item.data, i)}
+                        >
+                            <div className={`${styles.internal_member__contents}`}>
+                                <p className={styles.item__internal_member__name}>
+                                    {item.data.name}
+                                </p>
+                                <p className={styles.item__internal_member__details}>
+                                    {item.data.date.toLocaleTimeString([], {
+                                        hour: "2-digit",
+                                        minute: "2-digit"
+                                    })} - Selesai
+                                </p>
+                                <p className={styles.item__internal_member__details}>
+                                    Sem {item.data.year.map(year => `${convertToTerm(year, props.ganjil_genap)}`).join(", ")}
+                                </p>
+                                <p className={styles.item__internal_member__details}>
+                                    {item.data.major.map(maj => {
+                                        if (maj === "elektro") {
+                                            return "Elektro";
+                                        } else if (maj === "komputer") {
+                                            return "Tkom";
+                                        } else {
+                                            return "Biom";
+                                        }
+                                    }).join(", ")}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                })
+            )}
+            {isPopupVisible && activeItemData !== null && (
+                <div className={`${popupDirectionClass} ${styles.internal_member__popup}`}
+                     onMouseLeave={handleMouseLeave}
+                     ref={popupRef}>
+                    <div className={styles.internal_member__popup_content}>
+                        <div className={styles.popup__text}>
+                            <p>{activeItemData.date.toLocaleDateString("id-ID", {
+                                weekday: "long",
+                                day: "2-digit",
+                                month: "long"
+                            })}
+                            </p>
+                            <p> Pengasis:</p>
+                            <ul className={styles.popup__contents__person}>
+                                {activeItemData.person.map(person => (
+                                    <li>
+                                        {person.name}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className={styles.popup__seperator}></div>
+                        <div className={styles.popup__link}>
+                            {activeItemData.recordingsLink
+                                ? (<a href={activeItemData.recordingsLink}
+                                      target="_blank"
+                                      className={`${styles.content__link}`}>
+                                    Video
+                                    <svg className="svg-icon">
+                                        <use href="#youtube-logo"/>
+                                    </svg>
+                                </a>)
+                                : activeItemData.zoomMeetingsLink
+                                    ? (<a href={activeItemData.zoomMeetingsLink}
+                                          target="_blank"
+                                          className={`${styles.content__link}`}>
+                                        Zoom
+                                        <svg className="svg-icon">
+                                            <use href="#video-camera"/>
+                                        </svg>
+                                    </a>)
+                                    : (<div aria-disabled={true}
+                                            className={styles.link__No_link}>—</div>)
+                            }
+                        </div>
+                    </div>
+                </div>
+            )
+            }
+        </div>
+    )
+}
+
 export default function CalendarAsistensi(props: Props) {
-    type DisplayData = { date: Date, contents: Array<{ data: AsisData, isHighlighted: boolean }> };
+    const sameMajorAndTerm = (item: AsisData, options: typeof props.options) => {
+        const same_year = options.year ? item.year.includes(options.year) : true;
+        const same_major = options.major ? item.major.includes(options.major) : true;
+        return same_year && same_major;
+    }
 
     const groupedData = useMemo(() => {
         const groups = new Map<String, DisplayData>();
@@ -59,7 +253,10 @@ export default function CalendarAsistensi(props: Props) {
             const elementDateString = element.date.toISOString();
 
             if (groups.has(elementDateString)) {
-                groups.get(elementDateString)?.contents.push({data: element, isHighlighted: true});
+                groups.get(elementDateString)?.contents.push({
+                    data: element,
+                    isHighlighted: true
+                });
             } else {
                 groups.set(elementDateString, {
                     date: element.date,
@@ -67,7 +264,6 @@ export default function CalendarAsistensi(props: Props) {
                 });
             }
         })
-
 
         return Array.from(groups.values());
     }, [props.content]);
@@ -123,20 +319,41 @@ export default function CalendarAsistensi(props: Props) {
             output[data.date.getHours() - minHour][dateDiffInDays(minDate, data.date)] = data;
         })
 
-        console.table(output);
+        // console.table(output);
+
         return output;
     }, [props.content]);
 
+    const calendarMatrixHighlighted = useMemo(() => (
+        calendarMatrix.map(row => (
+            row.map(col => {
+                if (col) {
+                    return {
+                        ...col,
+                        contents: col.contents.map(i => {
+                            return {
+                                ...i,
+                                isHighlighted: sameMajorAndTerm(i.data, props.options),
+                            }
+                        })
+                    }
+                } else {
+                    return null;
+                }
+            })
+        ))
+    ), [props.content, props.options]);
+
 
     return (
-        <div className={styles.calendar_container}>
+        <section className={`${styles.calendar_container}`}>
             <div className={`${styles.calendar_container_fixed} `}>
                 <div className={`${styles.calendar_header_fixed} ${styles.calendar_header}`}>
                     <div className={`${styles.calendar_header_item_fixed} 
                                         ${styles.resize_handle_container}`}
                     >{"\u00A0"}
-                        <div className={styles.resize_handle_dummy}></div>
                     </div>
+                    <div className={styles.resize_handle_dummy}></div>
                 </div>
                 <div className={styles.calendar_row}>
                     <div className={`${styles.calendar_item_fixed__empty} ${styles.resize_handle_container}`}>
@@ -155,46 +372,28 @@ export default function CalendarAsistensi(props: Props) {
                 </div>
             </div>
             <div className={styles.calendar_container_scrollable}>
-                <div className={styles.calendar_header}>
-                    {arrDates.map(date => (
-                        <div className={`${styles.calendar_header__dates}`}>
+                {arrDates.map((date, dateIndex) => (
+                    <div className={styles.calendar_column}>
+                        <div
+                            className={`${styles.calendar_header} ${styles.calendar_header__contents} ${styles.calendar_header__dates}`}>
                             {date.toLocaleDateString("id-ID", {weekday: "short"})} - {" "}
                             {date.toLocaleDateString("id-ID", {month: "long", day: "numeric"})}
                         </div>
-                    ))}
-                </div>
-                <div className={styles.calendar_row}>
-                    <div className={styles.calendar_item__empty}>
-                        {calendarMatrix[0].map(() => (
-                            <div className={`${styles.calendar__contents_empty}`}>
-                            </div>
-                        ))}
-                    </div>
-                    {calendarMatrix.map(row => (
-                        <div className={styles.calendar_item}>
-                            {row.map((entry, i) => (
-                                <div className={`${styles.calendar__contents}`}>
-                                    {entry === null ? null : (
-                                        entry!.contents.map(item => (
-                                            <div className={`${styles.item__internal_member}`}>
-                                                {item.data.name}
-                                                <button
-                                                    className={`${styles.item__pop_up_button} `}
-                                                >
-                                                    <svg className={"svg-icon"}>
-                                                        <use href={"#arrow-right-circle"}/>
-                                                    </svg>
-                                                </button>
-                                            </div>
-                                        ))
-                                    )}
-                                </div>
-                            ))}
+                        <div className={`${styles.calendar_item__empty}`}>
+                            <div className={styles.calendar__contents_empty}></div>
                         </div>
-                    ))}
-                </div>
+                        {arrHours.map((_, hourIndex) => {
+                            const entry = calendarMatrixHighlighted[hourIndex][dateIndex];
+                            return (
+                                <div className={`${styles.calendar_item}`}>
+                                    <InternalMemberItem entry={entry} ganjil_genap={props.ganjil_genap}/>
+                                </div>
+                            )
+                        })}
+                    </div>
+                ))}
             </div>
-        </div>
+        </section>
     )
 }
 
